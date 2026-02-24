1d6f134ee17bb6bbaa80783118df074300d3bcbb
Othmanehaiba <othmanehaiba4@gmail.com>
2026-02-24 13:30:00 +0000

    adding the necessary migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,10 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('is_admin')->default(false);
+            $table->integer('reputation')->default(0);
+            $table->boolean('is_banned')->default(false);
+            $table->timestamp('banned_at')->nullable();
             $table->timestamps();
         });
 
---
+++ database/migrations/2026_02_24_103909_create_expenses_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('colocation_id')->constrained()->onDelete('cascade');
+            $table->foreignId('paid_by')->constrained('users')->onDelete('cascade');
+            $table->string('title');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 10, 2);
+            $table->date('expense_date');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
